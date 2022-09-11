6ed943c7bb02bcc811d8191d231267976208c2cf
alerafart <maalejandrarafart@gmail.com>
2022-09-11 15:36:55 +0200

    testing iframes

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,35 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Kurku</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/kurku_logo.png" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Kurku.tech!</a>
         </h1>
 
-        <p className="description">
+        {/* <p className="description">
           Get started by editing <code>pages/index.js</code>
-        </p>
+        </p> */}
 
+        <div className='grid'>
+          {/* <iframe width="356" height="200" src='http://www.youtube.com/watch?v=-n-B0IKmSvQ' frameborder="0" allowfullscreen>
+            Midi Controller
+          </iframe> */}
+          <iframe src="https://player.vimeo.com/video/332293586?h=9792aea725&color=ffffff&badge=0" width="640" height="272" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+          <p>
+            <a href="https://vimeo.com/332293586">SOUL FLYERS : THE FIRST SECOND</a> from <a href="https://vimeo.com/maximemoulin">MAXIME MOULIN</a> on <a href="https://vimeo.com">Vimeo
+            </a>.
+          </p>
+        </div>
         <div className="grid">
+          
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Video Title &rarr;</h3>
+            
+            <iframe src="https://player.vimeo.com/video/332293586?h=9792aea725&color=ffffff&badge=0" width="320" height="136" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <p>Video description Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
