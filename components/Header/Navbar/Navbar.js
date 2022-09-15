bce532641a792a164b7a281ba44436b5a2c0d77c
alerafart <maalejandrarafart@gmail.com>
2022-09-15 20:15:39 +0200

    navbar styling

--- components/Header/Navbar/Navbar.js
+++ components/Header/Navbar/Navbar.js
@@ -4,11 +4,14 @@ export default function Navbar() {
     return (
       
        <section className={styles.header}>
-        <div className={styles.header__logo}>logo</div>
+        {/* <div className={styles.header__logo}>logo</div> */}
+        {/* <Image className={styles.header__logo} src={logo}></Image> */}
+        {/* <img src="/vercel.svg" alt="Vercel" className="logo" /> */}
+        <img className={styles.header__logo} src="/images/kurku_logo.png" alt="Vercel" />
         <nav className={styles.header__nav}>
-            <a className={styles.header__nav__link}>link</a>
-            <a className={styles.header__nav__link}>link</a>
-            <a className={styles.header__nav__link}>link</a>
+            <a href='#' className={styles.header__nav__link}>link</a>
+            <a href='#' className={styles.header__nav__link}>link</a>
+            <a href='#' className={styles.header__nav__link}>link</a>
         </nav>
        </section>
         
