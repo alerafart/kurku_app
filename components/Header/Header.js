af4c3724f7ce265817b28d79bfe318429a6acb92
alerafart <maalejandrarafart@gmail.com>
2022-09-15 17:20:39 +0200

    responsive elements inside video with 4 breakpoints

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
         <Navbar />
         <h1 className={styles.header__title} > Kurku.App!</h1>
         <p className={styles.header__p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. ?</p>
-        <a className={styles.header__a} href="https://nextjs.org">Kurku.tech!</a>
+        <a className={styles.header__a} href="https://app.kurku.tech" target="_blank" >Get me there!</a>
       
       <video className={styles.header__iframe} src="https://player.vimeo.com/external/544030006.hd.mp4?s=04cde03295c6b6cd31aede65f0c6d2ad0b3614ad&profile_id=175" autoPlay loop />
         
