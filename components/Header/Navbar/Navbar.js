dce2003880d6bc7ee406e1a892d116eb643f22ce
alerafart <maalejandrarafart@gmail.com>
2022-09-16 16:27:15 +0200

    navbar logo wrapper

--- components/Header/Navbar/Navbar.js
+++ components/Header/Navbar/Navbar.js
@@ -7,7 +7,11 @@ export default function Navbar() {
         {/* <div className={styles.header__logo}>logo</div> */}
         {/* <Image className={styles.header__logo} src={logo}></Image> */}
         {/* <img src="/vercel.svg" alt="Vercel" className="logo" /> */}
-        <img className={styles.header__logo} src="/images/kurku_logo.png" alt="Vercel" />
+        <div className={styles.header__logo__wrapper}>
+            <img className={styles.header__logo} src="/images/kurku_logo.png" alt="Vercel" />
+        </div>
+
+       
         <nav className={styles.header__nav}>
             <a href='#' className={styles.header__nav__link}>link</a>
             <a href='#' className={styles.header__nav__link}>link</a>
