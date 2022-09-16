e2150768404da8daa7567fbb8a1558cc925b6b03
alerafart <maalejandrarafart@gmail.com>
2022-09-16 17:19:26 +0200

    navbar responsive adjustment

--- components/Header/Navbar/Navbar.js
+++ components/Header/Navbar/Navbar.js
@@ -4,14 +4,8 @@ export default function Navbar() {
     return (
       
        <section className={styles.header}>
-        {/* <div className={styles.header__logo}>logo</div> */}
-        {/* <Image className={styles.header__logo} src={logo}></Image> */}
-        {/* <img src="/vercel.svg" alt="Vercel" className="logo" /> */}
-        <div className={styles.header__logo__wrapper}>
             <img className={styles.header__logo} src="/images/kurku_logo.png" alt="Vercel" />
-        </div>
 
-       
         <nav className={styles.header__nav}>
             <a href='#' className={styles.header__nav__link}>link</a>
             <a href='#' className={styles.header__nav__link}>link</a>
