e51b31a2ace8ed6109ba43c20c95e266b0448349
alerafart <maalejandrarafart@gmail.com>
2022-09-18 20:01:05 +0200

    contact form organisation display wip

--- components/Footer/Contact_Form.js
+++ components/Footer/Contact_Form.js
@@ -1,10 +1,5 @@
 import { useState } from 'react';
 import styles from './footer.module.scss';
-//import '@fortawesome/fontawesome-svg-core/styles.css';
-//import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFaceRelieved } from '@fortawesome/pro-solid-svg-icons';
-// import handler from '../../pages/api/form';
 export default function Contact_Form() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -20,53 +15,50 @@ export default function Contact_Form() {
     console.log(data);
     
   };
-
   
   return (
     
       <section className={styles.footer__content__contact}> 
         <p>
-            
+            Contact Me
         </p>
         <form 
           action="/api/form"
           method="post"
           onSubmit={handleSubmit} 
           className={styles.footer__content__contact__form}>
-            <label className={styles.footer__content__contact__form__label} htmlFor="name">Name:</label>
-            <input
-              className={styles.footer__content__contact__form__input}
-                id="name"
-                type="text"
-                onChange={e => setName(e.target.value)}
-            />
-            <label className={styles.footer__content__contact__form__label}htmlFor="email">Email:</label>
-            <input
+            <div className={styles.footer__content__contact__form__senderDiv}>
+              <label className={styles.footer__content__contact__form__label} htmlFor="name">Name:</label>
+              <input
                 className={styles.footer__content__contact__form__input}
-                id="email"
-                type="email"
-                onChange={e => setEmail(e.target.value)}
-            />
-            <label className={styles.footer__content__contact__form__label} htmlFor="message">Message:</label>
-            <textarea className={styles.footer__content__contact__form__input}
-                id="message"
-                type="text"
-                rows="4"
-                onChange={e => setMessage(e.target.value)}
-            />
-            <button className= {styles.footer__content__contact__form__button} type="submit">Send</button>
-            </form>
+                  id="name"
+                  type="text"
+                  onChange={e => setName(e.target.value)}
+              />
+              {/* </div> */}
 
-{/* <form action="/api/form" method="post">
-  <label for="name">name name:</label>
-  <input type="text" id="name" name="name" />
-  <label for="email">email name:</label>
-  <input type="text" id="email" name="email" />
-  <label for="message">message name:</label>
-  <input type="text" id="message" name="message" />
-  <button type="submit">Submit</button>
-</form> */}
-
+              {/* <div className={styles.footer__content__contact__form__senderDiv}> */}
+              <label className={styles.footer__content__contact__form__label}htmlFor="email">Email:</label>
+              <input
+                  className={styles.footer__content__contact__form__input}
+                  id="email"
+                  type="email"
+                  onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+            
+            <div className={styles.footer__content__contact__form__textAreaDiv}>
+              <label className={styles.footer__content__contact__form__label} htmlFor="message">Message:</label>
+              <textarea className={styles.footer__content__contact__form__textArea}
+                  id="message"
+                  type="text"
+                  rows="4"
+                  onChange={e => setMessage(e.target.value)}
+              />
+              <button className= {styles.footer__content__contact__form__button} type="submit">Send</button>
+            </div>
+        </form>
+            
       </section>
     
   );
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,52 +1,48 @@
 import styles from './footer.module.scss';
 import Contact_Form from './Contact_Form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import '@fortawesome/fontawesome-svg-core/styles.css';
 
 export default function Footer() {
     return (
-      
       <footer className={styles.footer}>
         <div className={styles.footer__content}>
           <section
             className={styles.footer__content__social}>
             <form className={styles.footer__content__social__form}>
-                <label className={styles.footer__content__social__form__label}>
-                  <p className={styles.footer__content__social__network__p}>Subscribe to our Newsletter
-                  </p>
-                  <p><FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-regular fa-envelope" /></p>
-                </label>
-                  <input
-                  className={styles.footer__content__social__form__input}
-                    type='text'
-                    placeholder='enter email'>
-                  </input>
-              </form>
-            <div
-              className={styles.footer__content__social__network}>
-              
-              <div>
-                <p className={styles.footer__content__social__network__p}>Follow Us!
+              <label className={styles.footer__content__social__form__label}>
+                <p>Subscribe to our Newsletter
                 </p>
-                <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-facebook" />
-                <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-linkedin" />
-                <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-instagram" />
-                <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-twitter" />
+                <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-regular fa-envelope" />
+              </label>
+              <input
+              className={styles.footer__content__social__form__input}
+                type='text'
+                placeholder='enter email'>
+              </input>
+            </form>
+            <div className={styles.footer__content__social__network}>
+              <div>
+                <p>Follow Us!
+                </p>
+                <a href='#' target='_blank'>
+                  <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-facebook" />
+                </a>
+                <a href='#' target='_blank'>
+                  <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-linkedin" />
+                </a>
+                <a href='#' target='_blank'>
+                  <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-instagram" />
+                </a>
+                <a href='#' target='_blank'>
+                  <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-twitter" />
+                </a>
+                <a href='#' target='_blank'>
                 <FontAwesomeIcon className={styles.footer__content__social__network__img} icon="fa-brands fa-youtube" />
+                </a>
               </div>
             </div>
           </section>
           <Contact_Form />
-          
-          {/* <section className={styles.footer__contact}>
-            <form action="/send-data-here" method="post">
-              <label for="first">First name:</label>
-              <input type="text" id="first" name="first" />
-              <label for="last">Last name:</label>
-              <input type="text" id="last" name="last" />
-              <button type="submit">Send</button>
-            </form>
-          </section> */}
         </div>
         <div className={styles.footer__mentions}>
           <a
@@ -58,6 +54,6 @@ export default function Footer() {
             <img src="/images/kurku_logo.png" alt="Vercel" className="logo" />
           </a>
         </div>
-    </footer>        
+      </footer>        
     )
   }
