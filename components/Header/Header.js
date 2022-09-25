797470c1632da6d7bd5ed80e655b3dd8c17ed0bb
alerafart <maalejandrarafart@gmail.com>
2022-09-25 20:31:42 +0200

    responsive inputs resizing

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
         <p className={styles.header__p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. ?</p>
         <a className={styles.header__a} href="https://app.kurku.tech" target="_blank" >Get me there!</a>
       
-      <video className={styles.header__iframe} src="https://player.vimeo.com/external/544030006.hd.mp4?s=04cde03295c6b6cd31aede65f0c6d2ad0b3614ad&profile_id=175" autoPlay loop controlsList='play' muted/>
+      <video className={styles.header__iframe} src="https://player.vimeo.com/external/544030006.hd.mp4?s=04cde03295c6b6cd31aede65f0c6d2ad0b3614ad&profile_id=175" autoPlay loop  controls controlsList='play' muted/>
         
        </section>
         
