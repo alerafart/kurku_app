2b5199e010f57562ea4f0eb9bd3e2374d6945556
alerafart <maalejandrarafart@gmail.com>
2022-09-27 17:20:05 +0200

    more comments cleaning

--- pages/index.js
+++ pages/index.js
@@ -44,73 +44,9 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
         
         .logo {
           height: 1em;
-        }
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
         }
       `}</style> */}
 
@@ -121,61 +57,43 @@ export default function Home() {
               padding: 0;
               margin: 0;;
               font-family: 'Oswald', sans-serif;
-              // font-family: 'Archivo Narrow', sans-serif;
+              //background-color:red;
           }
+          
           .container {
-           // min-height: 100vh;
+            // width:100%;
+            
+            // min-height: 100vh;
             // padding: 0 0.5rem;
             // display: flex;
             // flex-direction: column;
             // justify-content: center;
-            // align-content:center;
             // align-items: center;
-            color:red;
           }
           h1 {
             font-weight: bold;
-            // font-family: 'Archivo Narrow', sans-serif;
-            // font-family: 'Barlow Condensed', sans-serif;
-            //font-family: 'Oswald', sans-serif;
           }
           h2 {
-            // font-family: 'Oswald', sans-serif;
             font-family: 'Archivo Narrow', sans-serif;
-            //font-family: 'Barlow Condensed', sans-serif;
             width: 95%;
             text-align: left;
             color: black;
-          // font-family: Helvetica,Arial,sans-serif;
             font-size: 2.5rem;
-          //  font-weight: bold;
             text-transform: uppercase;
             letter-spacing: 1.5rem;
           }
           h3 {
-          // font-family: Helvetica,Arial,sans-serif;
             font-weight:600;
             text-transform: uppercase;
           }
-          p {
-            // font-family: Helvetica,Arial,sans-serif;
-          }
+
           * {
               box-sizing: border-box;
           }
 
-          .container {
-              min-height: 100vh;
-              width: 100%;
-              padding: 0 0.5rem;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-            }
-
             main {
-              width: 100%;
+              //background-color:grey;
+              //width: 100%;
               flex: 1;
               display: flex;
               flex-direction: column;
@@ -211,8 +129,6 @@ export default function Home() {
               color: inherit;
               text-decoration: none;
               font-family: 'Barlow Condensed', 'Oswald', sans-serif;
-              // font-family: 'Archivo Narrow', sans-serif;
-              //font-family: 'Oswald', sans-serif;
             }
 
             .grid {
