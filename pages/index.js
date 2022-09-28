68e6530b1d630ad5d575642ade334cf34664abea
alerafart <maalejandrarafart@gmail.com>
2022-09-28 15:50:13 +0200

    adjustings, some details still remaining

--- pages/index.js
+++ pages/index.js
@@ -57,27 +57,18 @@ export default function Home() {
               padding: 0;
               margin: 0;;
               font-family: 'Oswald', sans-serif;
-              //background-color:red;
               width:100%;
           }
           
           .container {
-            align-self: center;
-            width: 100vw;
-        height: auto;
-            // width:100vw;
-            width:100%;
-            flex: 1;
-            background-color:red;
-            // min-height: 100vh;
-            // padding: 0 0.5rem;
-            // display: flex;
-            // flex-direction: column;
-            // justify-content: center;
-            // align-items: center;
+            // width: 100vw;
+            // flex: 1;
+            // margin-left: auto;
+            // margin-right: auto;
+            // align-self:stretch;
+            
           }
           main {
-            //background-color:grey;
             width: 100%;
            // flex: 1;
             display: flex;
@@ -93,7 +84,6 @@ export default function Home() {
             width: 95%;
             text-align: left;
             color: black;
-            font-size: 2.5rem;
             text-transform: uppercase;
             letter-spacing: 1.5rem;
           }
@@ -105,46 +95,11 @@ export default function Home() {
           * {
               box-sizing: border-box;
           }
-
             
-            
-            footer {
-              width: 100%;
-              height: fit-content;
-              border-top: 1px solid #eaeaea;
-              display: flex;
-              justify-content: space-around;
-              align-items: center;
-            }
-
-            footer img {
-              margin-left: 0.5rem;
-              background-color: #f5f5f5;
-              padding: 2px;
-            }
-
-            footer a {
-
-            }
-            footer p {
-              text-transform: uppercase;
-              font-weight: bold;
-              padding: 0.2rem;
-              
-            }
             a {
               color: inherit;
               text-decoration: none;
               font-family: 'Barlow Condensed', 'Oswald', sans-serif;
-            }
-
-            .grid {
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              flex-wrap: wrap;
-              max-width: 800px;
-              margin-top: 3rem;
             }
 
             .logo {
@@ -163,7 +118,6 @@ export default function Home() {
               border-radius: 5px;
               z-index:1;
               margin: 1rem;
-            
             }
             #backToTop:hover {
               color:rgb(62, 140, 187);
